b04a58ff1233cfdca76491d8a0f42d135d959d0f
Максим <90824803+IxMedvedxI@users.noreply.github.com>
2022-04-22 02:04:32 +0300

    test

--- v9_final/ConsoleApplication9/ConsoleApplication9.cpp
+++ v9_final/ConsoleApplication9/ConsoleApplication9.cpp
@@ -16,29 +16,5 @@ int main()
 	Train t("", xin, 0);
 	cout << "\n"<<xin<<" " <<vokzal[vokzal.poisk(t)];
 
-	
 
-
-
-	/*int arr[5] = { 1, 2 ,3,4,5 };
-	Arr <int> myArr(arr, 5);
-	cout << myArr;
-	try {
-		myArr[0] = 10;
-		myArr[1] = 20;
-		myArr[4] = 100;
-		myArr[3] = -5;
-		myArr[20] = 7;
-	}
-	catch (int n) {
-		if (n == 1)
-			cout << "bad index\n";
-	}
-	cout << myArr;
-	cout << "\n";
-	ifstream in("Text.txt");
-	in >> myArr;
-	myArr.sort();
-	cout << myArr,"\n";
-	*/
 }
