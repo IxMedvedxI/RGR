39c8636d81ec77670a81fb2615d663b255d4605b
Максим <90824803+IxMedvedxI@users.noreply.github.com>
2022-04-22 02:09:51 +0300

    Add files via upload

---
+++ v9_final/v9.1/ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,30 @@
+﻿
+#include "array.h"
+#include "Train.h"
+
+void print(MyArr<Train> vokzal, int xin) {
+	int fg = 1;
+	for (int i = 0; i < vokzal.len(); i++) {
+		if (xin == vokzal[i].getNumber()) {
+			cout << vokzal[i]<<"\n";
+			fg = 0;
+		}
+	}
+	if (fg) {
+		cout << "Not Trains this number";
+	}
+}
+
+int main()
+{
+	int xin;
+	MyArr<Train> vokzal;
+	ifstream in("Text1.txt");
+	ofstream out("Text2.txt");
+	in >> vokzal;
+	vokzal.sort(Train::compAll);
+	out << vokzal;
+	cin >> xin;
+	print(vokzal,xin);
+
+}
